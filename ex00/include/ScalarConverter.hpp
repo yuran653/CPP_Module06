0a41c7f3ba7b031600ec40a4ae803b61594bdb78
Iurii Ovsiannikov <jgoldste@z2t17c3.42bangkok.com>
2023-10-28 18:34:38 +0700

    ex00_retry_Oct28_18-34

---
+++ ex00/include/ScalarConverter.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 18:06:02 by jgoldste          #+#    #+#             */
+/*   Updated: 2023/10/28 18:20:55 by jgoldste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <cctype>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <sstream>
+#include <string>
+
+class ScalarConverter {
+	private:
+		~ScalarConverter();
+	
+	public:
+		static void	convert(std::string input);
+
+};
+
+#endif
---
+++ ex00/src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 18:06:05 by jgoldste          #+#    #+#             */
+/*   Updated: 2023/10/28 18:20:10 by jgoldste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int main (int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr << "Error: Wrong number of arguments" << std::endl;
+		return 1;
+	} else {
+		try {
+			ScalarConverter::convert(argv[1]);
+		} catch (std::invalid_argument& e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+			return 1;
+		}
+	}
+	return 0;
+}
